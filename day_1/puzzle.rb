5eafc20dc81bc7b95205ae35b3e0a4912df2c490
Caio Garcia <caioif4@gmail.com>
2024-03-27 00:04:20 -0300

    day 1 pt 2 working

--- day_1/puzzle.rb
+++ day_1/puzzle.rb
@@ -1,5 +1,5 @@
-# require "debug"
-# codes = File.read('input.txt').split("\n")
+require "debug"
+codes = File.read('codes.txt').split("\n")
 
 def str_to_number(str)
   hash_numbers = {
@@ -16,20 +16,38 @@ def str_to_number(str)
   positions_arr = []
   hash_numbers.each do |key, value|
     str.enum_for(:scan, /#{key}/).map do
-      positions_arr << { 
+      positions_arr << {
         key => Regexp.last_match.begin(0)
       }
     end
   end
   positions_arr.sort_by! { |hsh| hsh.values }
+
+  numbers = str.scan(/\d/).join("").split("")
+
+  first_number_position = str.index(numbers[0]) rescue nil
+  last_number_position = str.size - str.reverse.index(numbers[-1]) rescue nil
+
   first = positions_arr.first
   last = positions_arr.last
-
-  puts str
-  puts positions_arr
-  # aqui ta errado temq ue substituir na posicao certa
-  if !positions_arr.empty?
+  # debugger if str == '422268'
+  unless positions_arr.empty? or (first_number_position && first_number_position < first.first[1])
     str.sub!(/#{first.keys.first}/, hash_numbers[first.keys.first].to_s)
+  end
+  unless positions_arr.empty? or (last_number_position && last_number_position > last.first[1])
+    str.sub!(/#{last.keys.first}/, hash_numbers[last.keys.first].to_s)
+  end
+  if !positions_arr.empty?
+    if first_number_position && first_number_position < first.first[1]
+      if last_number_position && last_number_position > last.first[1]
+      else
+        if first != last
+          str.gsub!(/#{last.keys.first}/, hash_numbers[last.keys.first].to_s)
+        end
+      end
+    else
+      str.sub!(/#{first.keys.first}/, hash_numbers[first.keys.first].to_s)
+    end
     if (first.values.first + first.keys.first.size) < (last.values.first + 1) && first != last
       str.gsub!(/#{last.keys.first}/, hash_numbers[last.keys.first].to_s)
     end
@@ -37,22 +55,20 @@ def str_to_number(str)
   str
 end
 
-# sum = 0
-# codes.map do |code|
-#   puts code
-#   sanitazed_code = str_to_number(code)
-#   numbers = sanitazed_code.scan(/\d/).join("")
-#   puts numbers
-#   first_number = numbers[0]
-#   last_number = numbers[-1]
-#   calibration_code = "#{first_number}#{last_number}".to_i
-#   puts calibration_code
-#   puts "\n"
-#   sum += calibration_code
-# end
+sum = 0
+codes.map do |code|
+  puts code
+  sanitazed_code = str_to_number(code)
+  numbers = sanitazed_code.scan(/\d/).join("")
+  puts numbers
+  first_number = numbers[0]
+  last_number = numbers[-1]
+  calibration_code = "#{first_number}#{last_number}".to_i
+  puts calibration_code
+  puts "\n"
+  sum += calibration_code
+end
 
-# #54714 errado
-# puts sum
+puts sum
 
-# a = str_to_number("4twone")
-p a
+# p str_to_number("eightwothree")
