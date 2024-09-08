a24080f50da000621d698bdc9fd252558feafa91
Caio Garcia <caiogarcia@hey.com>
2024-09-07 22:11:23 -0300

    day 2 pt 2 working

--- day_2/puzzle.rb
+++ day_2/puzzle.rb
@@ -1,19 +1,24 @@
 require 'debug'
 input = File.read('input.txt').split("\n")
-red = 12
-green = 13
-blue = 14
+# red = 12
+# green = 13
+# blue = 14
 sum = 0
 input.each do |game|
   game_id = game.split(",").first.to_i
   valid_game = true
+  red = 0
+  green = 0
+  blue = 0
   game.split(",")[1..-1].join(",").split(";").each do |grab|
     grab.split(",").each do |gems|
       color = gems.scan(/[a-zA-Z]+/).first
       count = gems.scan(/\d/).join("").to_i
-      valid_game = false if (count > eval(color)) rescue binding.irb
+      if count > eval(color)
+        eval("#{color} = #{count}")
+      end
     end
   end
-  sum += game_id if valid_game
+  sum += (red * green * blue)
 end
 puts sum
